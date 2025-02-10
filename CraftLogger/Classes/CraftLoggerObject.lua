0f3347705908fb0a21465f182b51577c3d3771a3
MasterOfMagic16 <brmarcy2021@gmail.com>
2025-02-09 20:01:38 -0500

    Delete CraftLogger directory
    Wrong Location

--- CraftLogger/Classes/CraftLoggerObject.lua
+++
@@ -1,6 +0,0 @@
-local CraftLogger = select(2, ...)
-
-CraftLogger.CraftLoggerObject = CraftLogger.Object:extend()
-
-function CraftLogger.CraftLoggerObject:new()
-end
--- CraftLogger/Init/GLibs.lua
+++
@@ -1,3 +0,0 @@
-local CraftLogger = select(2, ...)
-
-CraftLogger.GUTIL = LibStub:GetLibrary("GUTIL-2.0")

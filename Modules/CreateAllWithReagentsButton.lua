c7f3ed4a528f8a034de511c46a57d730582ddb67
MasterOfMagic16 <brmarcy2021@gmail.com>
2025-03-23 02:49:13 -0400

    Refine
    Refined code but still trying to update craftable amount

--- Modules/CreateAllWithReagentsButton.lua
+++ Modules/CreateAllWithReagentsButton.lua
@@ -9,20 +9,18 @@ local GUTIL = CraftLogger.GUTIL
 
 CraftLogger.CreateAllWithReagentsButton = {}
 
-function CLCraftTest()
-	print("Called")
+local initialized = false
+function CraftLogger.CreateAllWithReagentsButton:Init()
+	if initialized then return end
 	
-	--The only possible issue is that this might not craft what is expected.
-	--However, what is actually crafted will still match CraftLogger, so good.
-	local recipeData = CraftSimAPI:GetCraftSim().INIT.currentRecipeData:Copy()
+	local craftableAmount
 
-	local craftAbleAmount = max(1, recipeData.reagentData:GetCraftableAmount(recipeData:GetCrafterUID()))
-	recipeData:Craft(craftableAmount)
-end
+	local function Init()
+		local recipeData = CraftSimAPI:GetCraftSim().INIT.currentRecipeData:Copy()
+		craftableAmount = max(1, recipeData.reagentData:GetCraftableAmount(recipeData:GetCrafterUID()))
+		CraftLogger.CreateAllWithReagentsButton.Button.label = "Create All With Reagents [" .. craftableAmount .. "]"
+	end
 
---Make it update craftable amount
---Doesn't work for orders, but fine since one craft anyway
-function CraftLogger.CreateAllWithReagentsButton:Init()	
 	CraftLogger.CreateAllWithReagentsButton.Button = GGUI.Button {
 		parent = ProfessionsFrame.CraftingPage.CreateAllButton,
         anchorPoints = { {
@@ -30,13 +28,22 @@ function CraftLogger.CreateAllWithReagentsButton:Init()
             anchorA = "RIGHT", anchorB = "LEFT", offsetX = -10,
         } },
         adjustWidth = true,
-        label = "Create All With Reagents []",
+		label = "Create All With Reagents []",
         tooltipOptions = {
             anchor = "ANCHOR_CURSOR_RIGHT",
             text = "Create All Using Only Current Reagent Configuration",
         },
-        clickCallback = function() CLCraftTest() end,
+        clickCallback = function() recipeData:Craft(craftableAmount) end,
     }
 	
+	Button = CraftLogger.CreateAllWithReagentsButton.Button
+	
+	Init()
+	
+	local hookFrame = ProfessionsFrame.CraftingPage.SchematicForm
+	hooksecurefunc(hookFrame, "Init", Init)
+	
 	CraftLogger.CreateAllWithReagentsButton.Button:Show()
+	
+	initialized = true
 end

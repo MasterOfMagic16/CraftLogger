fffa1e657e5529241588c238397f96c3a0f727df
MasterOfMagic16 <brmarcy2021@gmail.com>
2025-04-12 00:43:03 -0400

    Adds Items

--- Classes/ItemTracker.lua
+++ Classes/ItemTracker.lua
@@ -4,8 +4,33 @@ local systemPrint = print
 
 local GUTIL = CraftLogger.GUTIL
 
+print("ItemTracker Loaded")
+
 CraftLogger.ItemTracker = CraftLogger.CraftLoggerObject:extend()
 
-function CraftLogger.ItemTracker:new()
+function CraftLogger.ItemTracker:new(name, quantity, price)
+	self.name = name
+	self.quantity = quantity or 0
+	self.value = self.quantity * (price or 0)
+end
+
+function CraftLogger.ItemTracker:GetPrice()
+	if self.quantity == 0 then
+		return 0
+	end
+	return self.value / self.quantity
+end
+
+function CraftLogger.ItemTracker:Add(quantity, price)
+	local gainedValue = quantity * (price or 0)
 	
+	self.quantity = self.quantity + quantity 
+	self.value = self.value + gainedValue
+end
+
+function CraftLogger.ItemTracker:Subtract(quantity)
+	local lostValue = quantity * self:GetPrice()
+	
+	self.quantity = self.quantity - quantity 
+	self.value = self.value - lostValue
 end
--- Modules/ValueTracker.lua
+++ Modules/ValueTracker.lua
@@ -4,4 +4,20 @@ local systemPrint = print
 
 local GUTIL = CraftLogger.GUTIL
 
-CraftLogger.ValueTracker = {}
+print("ValueTracker Loaded")
+
+CraftLogger.ValueTracker = GUTIL:CreateRegistreeForEvents({ "AUCTION_HOUSE_SHOW_COMMODITY_WON_NOTIFICATION", "TRADE_SKILL_ITEM_CRAFTED_RESULT" })
+
+CLItemsTracked = {}
+
+function CraftLogger.ValueTracker:AUCTION_HOUSE_SHOW_COMMODITY_WON_NOTIFICATION(commodityName, commodityQuantity)
+	print("Triggered")
+	
+	itemTracker = GUTIL:Find(CLItemsTracked, function(itemTracker) return itemTracker.name == commodityName end)
+	if itemTracker then
+		itemTracker:Add(commodityQuantity)
+	else
+		itemTracker = CraftLogger.ItemTracker(commodityName, commodityQuantity)
+		table.insert(CLItemsTracked, itemTracker)
+	end
+end
